8e09a5fddc3883620fad28b3eb8644c32d5602d4
Daniel Lundin <daniel.lundin@expressen.se>
2015-08-19 13:43:07 +0200

    Added build script in package.json

--- build/react-material-icons.js
+++ build/react-material-icons.js
@@ -1,6 +1,12 @@
-var MorphIcon = React.createClass({displayName: "MorphIcon",
+'use strict';
 
-  getInitialState:function() {
+var React = require('react'),
+    SVGMorpheus = require('../lib/svg-morpheus.js');
+
+var MorphIcon = React.createClass({
+  displayName: 'MorphIcon',
+
+  getInitialState: function getInitialState() {
     this.shapes = {
       // materialdesignicons.com
       'amazon': '<path d="M13.23 10.56V10c-1.94 0-3.99.39-3.99 2.67 0 1.16.61 1.95 1.63 1.95.76 0 1.43-.47 1.86-1.22.52-.93.5-1.8.5-2.84m2.7 6.53c-.18.16-.43.17-.63.06-.89-.74-1.05-1.08-1.54-1.79-1.47 1.5-2.51 1.95-4.42 1.95-2.25 0-4.01-1.39-4.01-4.17 0-2.18 1.17-3.64 2.86-4.38 1.46-.64 3.49-.76 5.04-.93V7.5c0-.66.05-1.41-.33-1.96-.32-.49-.95-.7-1.5-.7-1.02 0-1.93.53-2.15 1.61-.05.24-.25.48-.47.49l-2.6-.28c-.22-.05-.46-.22-.4-.56.6-3.15 3.45-4.1 6-4.1 1.3 0 3 .35 4.03 1.33C17.11 4.55 17 6.18 17 7.95v4.17c0 1.25.5 1.81 1 2.48.17.25.21.54 0 .71l-2.06 1.78h-.01"/><path d="M20.16 19.54C18 21.14 14.82 22 12.1 22c-3.81 0-7.25-1.41-9.85-3.76-.2-.18-.02-.43.25-.29 2.78 1.63 6.25 2.61 9.83 2.61 2.41 0 5.07-.5 7.51-1.53.37-.16.66.24.32.51"/><path d="M21.07 18.5c-.28-.36-1.85-.17-2.57-.08-.19.02-.22-.16-.03-.3 1.24-.88 3.29-.62 3.53-.33.24.3-.07 2.35-1.24 3.32-.18.16-.35.07-.26-.11.26-.67.85-2.14.57-2.5z"/>',
@@ -786,15 +792,9 @@ var MorphIcon = React.createClass({displayName: "MorphIcon",
       'star_outline': '<path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"/>',
 
       // Additional
-      'live_circle': '<path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zM4 9.094h1.188v4.844h2.53v.968H4V9.094zm4.5 0h1.188v5.812H8.5V9.094zm1.78 0h1.345l1.28 4.375 1.345-4.377h1.313l-2 5.812h-1.25l-2.033-5.81zm5.845 0H20v.97l-2.688-.002v1.376h2.282v.937h-2.282v1.563H20v.968h-3.875V9.094z"/>',
+      'live_circle': '<path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zM4 9.094h1.188v4.844h2.53v.968H4V9.094zm4.5 0h1.188v5.812H8.5V9.094zm1.78 0h1.345l1.28 4.375 1.345-4.377h1.313l-2 5.812h-1.25l-2.033-5.81zm5.845 0H20v.97l-2.688-.002v1.376h2.282v.937h-2.282v1.563H20v.968h-3.875V9.094z"/>'
     };
     return {};
-  },
-
-  componentDidMount:function () {
-    var box = React.findDOMNode(this.refs.svgBox);
-    var options = this.props.options ? this.props.options : {};
-    this.Morph = new SVGMorpheus(box, options);
   },
 
   propTypes: {
@@ -802,33 +802,37 @@ var MorphIcon = React.createClass({displayName: "MorphIcon",
     custom: React.PropTypes.bool,
     size: React.PropTypes.number,
     shapes: React.PropTypes.object,
-    options: React.PropTypes.object,
+    options: React.PropTypes.object
   },
 
-  to:function(icon) {
+  componentDidMount: function componentDidMount() {
+    var box = React.findDOMNode(this.refs.svgBox);
+    var options = this.props.options ? this.props.options : {};
+    this.Morph = new SVGMorpheus(box, options);
+  },
+
+  to: function to(icon) {
     this.Morph.to(icon);
   },
 
-  makeIcons:function(shapes) {
-    return this.props.icons.map(function(icon)  {
-      return React.createElement("g", {id: icon, dangerouslySetInnerHTML: {__html: shapes[icon]}})
+  makeIcons: function makeIcons(shapes) {
+    return this.props.icons.map(function (icon) {
+      return React.createElement('g', { id: icon, dangerouslySetInnerHTML: { __html: shapes[icon] } });
     });
   },
 
-  render:function() {
-    var isArray=function(val)  { return Object.prototype.toString.call(val) == '[object Array]'; }
+  render: function render() {
+    if (this.props.custom == true) var icons = this.makeIcons(this.props.shapes);else var icons = this.makeIcons(this.shapes);
 
-    if (!this.props.icons || !isArray(this.props.icons))
-      console.error(new Error('Icons property must be array type'));
-
-    if (this.props.custom == true) var icons = this.makeIcons(this.props.shapes);
-    else var icons = this.makeIcons(this.shapes);
-
-    return (
-      React.createElement("svg", {ref: "svgBox", xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 24 24",
+    return React.createElement(
+      'svg',
+      { ref: 'svgBox', xmlns: 'http://www.w3.org/2000/svg', viewBox: '0 0 24 24',
         width: this.props.size || 25, height: this.props.size || 25,
         style: this.props.style
-      }, icons)
+      },
+      icons
     );
   }
 });
+
+module.exports = MorphIcon;
